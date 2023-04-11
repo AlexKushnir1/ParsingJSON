4ff44db82f12a45c294f42ef56d215253782f48c
AlexKushnir1 <127443695+AlexKushnir1@users.noreply.github.com>
2023-04-11 19:50:14 +0300

    Add files via upload

---
+++ NBU_Rusl/src/main/java/com/example/nbu_rusl/DataOfMonth.java
@@ -0,0 +1,29 @@
+package com.example.nbu_rusl;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataOfMonth {
+    private int dt;
+    private String txt;
+    private String txten;
+    private String id_api;
+    private int leveli;
+    private String parent;
+    private String freq;
+    private float value;
+    private String tzep;
+
+    public int getDate() {
+        return dt;
+    }
+
+    public String getId_api() {
+        return id_api;
+    }
+
+
+    public float getValue() {
+        return value;
+    }
+}
---
+++ NBU_Rusl/src/main/java/com/example/nbu_rusl/JsonSimpleParser.java
@@ -0,0 +1,27 @@
+package com.example.nbu_rusl;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+@Component
+public class JsonSimpleParser {
+    public String parse(URL url) throws IOException {
+      HttpURLConnection connection = (HttpURLConnection)  url.openConnection();
+      connection.setRequestMethod("GET");
+        InputStream inputStream = connection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line =bufferedReader.readLine())!=null){
+            response.append(line);
+        }
+        bufferedReader.close();
+        connection.disconnect();
+        return response.toString();
+    }
+}
